47b921fdac41b9ee56e9279f1f0e0e80c7e2bfac
Anand Tiwari <167313054+atsrf2@users.noreply.github.com>
2025-04-07 00:08:58 +0530

    Create variables.tf
    Variable inpur for project ID, region and service name

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "project_id" {}
+variable "region_a" { default = "us-central1" }
+variable "region_b" { default = "europe-west1" }
+variable "service_name_a" { default = "service-a" }
+variable "service_name_b" { default = "service-b" }
